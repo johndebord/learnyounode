3ca03818c174139f66a1c756bd9ce22b8824496b
johndebord <i@johndebord.tk>
2018-01-27 19:01:18 -0600

    Finished exercises on two modules.

--- program.js
+++ program.js
@@ -220,3 +220,154 @@ http.get(url, function callback(response) {
     })
 })
 */
+
+/* Exercise 8
+var http = require("http");
+var bl = require('bl')
+
+var url = process.argv[2];
+
+http.get(url, function callback(response) {
+    response.pipe(bl(function (err, data) {
+        if(err) return console.error(err);
+        data = data.toString();
+        console.log(data.length);
+        console.log(data);
+    }))
+})
+*/
+
+/* Exercise 9 : read up on ansynchronous javascript and counting callbacks.
+var http = require("http");
+var bl = require("bl");
+var url = [process.argv[2], process.argv[3], process.argv[4]];
+var res = [];
+
+var count = 0;
+for (var i = 0; i != url.length; ++i) {
+    http.get(url[i], function(index) {
+        return function(response) {
+            response.pipe(bl(function(err,data) {
+                if(err) return console.log(err);
+                res.push(data.toString()); 
+                ++count;   
+                if (count == 3) {
+                    for (var j = 0; j != res.length; ++j) {
+                        console.log(res[j])
+                    }
+                }
+            }))
+        }
+    }(i))
+}
+*/
+
+/* Exercise 10
+var net = require("net");
+
+var date = new Date();
+
+var server = net.createServer(function(socket) {
+    var data = date.getFullYear() + '-' + date.getMonth() + 1 + '-' + date.getDate() + ' '+ date.getHours() + ':' + date.getMinutes();
+    socket.write(data);
+    socket.end('\n');
+});
+server.listen(process.argv[2]);k
+*/
+
+/* Exercise 11
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res) {
+    fs.createReadStream(process.argv[3]).pipe(res);
+});
+server.listen(process.argv[2]);
+*/
+
+/* Exercise 12
+var http = require("http");
+var map = require("through2-map");
+
+var server = http.createServer(function(req, res) {
+    req.pipe(map(function (data) {
+        return data.toString().toUpperCase();
+    })).pipe(res);
+});
+server.listen(process.argv[2]);
+*/
+
+/* Exercise 13 : read up on this one.
+var http = require("http");
+var url = require("url");
+
+function formatTime(date) {
+    return {
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds()
+    };
+}
+
+function fomatUnix(date) {
+    return {
+        unixtime: date.getTime()
+    };
+}
+
+function processRequest(urlPath, date) {
+    var result = {};
+    if (urlPath === "/api/parsetime") {
+        result = formatTime(date);
+    } else if (urlPath === "/api/unixtime") {
+        result = fomatUnix(date);
+    }
+    return result;
+}
+
+var server = http.createServer(function (req, res) {
+    var urlInfos = url.parse(req.url, true);
+    var date = new Date(urlInfos.query.iso);
+    var data = processRequest(urlInfos.pathname, date);
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(data));
+});
+server.listen(process.argv[2]);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
